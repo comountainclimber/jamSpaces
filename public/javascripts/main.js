421b368a1868d8772a243cbb03f60664541e7e6d
comountainclimber <mlasky46@gmail.com>
2015-10-28 17:15:38 -0600

    ch ch ch ch changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -43,47 +43,6 @@ angular.module('App')
 angular.module('App')
 	.controller('jamController', ['$scope', '$http','$routeParams', '$location', '$rootScope', function($scope, $http, $routeParams, $location, $rootScope){
 		
-		// $scope.armMidi = function() {
-		// 	console.log("success!")
-		// 	Wad.midiInstrument = new Wad({source : 'sawtooth'});
-		// }
-		
-		// console.log(Wad.midiInputs[0])
-
-		if (Wad.midiInputs[0]) {
-			Wad.midiInputs[0].onmidimessage = function(event){
-				console.log(event.data)
-				socket.emit('midiData', {midiData:event.data, destination:$routeParams.jamDestination})	
-		}
-		}
-
-		// socket.on()
-
-
-		var hat = document.getElementById('highHat')
-		var kick = document.getElementById('kick')
-		var snare = document.getElementById('snare')
-
-		$scope.notePlayed = function(event){
-			$scope.inputs = event.which
-
-			if (event.which === 116){
-				hat.play()
-			}
-
-			if (event.which === 114){
-				kick.play()
-			}
-
-			if (event.which === 121){
-				snare.play()
-			}
-
-
-			socket.emit('notebeingplayed', { notes: $scope.inputs, destination: $routeParams.jamDestination})
-		}
-
-
 		$scope.destination = $routeParams.jamDestination
 
 		var socket = io()
@@ -93,51 +52,148 @@ angular.module('App')
 			console.log("user connected to " + data + " jamSpace")
 		})
 
+
+//midi output handler and emmission-----------------------
+		if (Wad.midiInputs[0]) {
+			Wad.midiInputs[0].onmidimessage = function(event){
+				console.log(event.data)
+				socket.emit('midiData', {midiData:event.data, destination:$routeParams.jamDestination})	
+		}
+		}
+
+//the drum machine------------------------------------
+		var hat = document.getElementById('highHat')
+		var kick = document.getElementById('kick')
+		var snare = document.getElementById('snare')
+
+		$scope.notePlayed = function(event){
+			$scope.inputs = event.which
+			if (event.which === 116){
+				hat.play()
+			}
+			if (event.which === 114){
+				kick.play()
+			}
+			if (event.which === 121){
+				snare.play()
+			}
+		socket.emit('notebeingplayed', { notes: $scope.inputs, destination: $routeParams.jamDestination})
+		}
+
+
+	
+//handling incoming drum machine "notes"------------------------------------
 		socket.on('music', function(data){
 			console.log(data.notes)
 			if (data.notes === 116){
 			hat.play()
 			}
-
 			if (data.notes === 114){
 				kick.play()
 			}
-
 			if (data.notes === 121){
 				snare.play()
 			}
 		})
 
+
+//midi input (from server) handler and emmission-----------------------
 		socket.on('midi', function(data){
 			console.log(data[1])
-
 			if ( data[0] === 144 ) {
 				console.log("success!")
 				// Wad.midiInstrument = new Wad({source : 'sawtooth'})
-
 			 if ( data[2] === 0 ) { // noteOn velocity of 0 means this is actually a noteOff message
                 console.log('|| stopping note: ', Wad.pitchesArray[data[1]-12]);
                 Wad.midiInstrument.stop(Wad.pitchesArray[data[1]-12]);
             }
-
 			else if ( data[1] > 0 ) {
 			Wad.midiInstrument = new Wad({source : 'sawtooth'})
             console.log('> playing note: ', Wad.pitchesArray[data[1]-12]);
             Wad.midiInstrument.play({pitch : Wad.pitchesArray[data[1]-12], label : Wad.pitchesArray[data[1]-12], callback : function(that){
             }})
-
-            
-        }
-
-
-
-
-				// Wad.midiInstrument.play()
-			}
+    		}}
 		})
+
+//click handler for keyboard-----------------------
+		Wad.midiInstrument = new Wad({source : 'sawtooth'})
+		// $scope.release = false
+		// $scope.keyboardClickRelease = function(event){
+		// 	console.log(event)
+		// 	$scope.release = true
+		// }
+		$scope.keyboardClickRelease = function () {
+			Wad.midiInstrument.stop()
+		}
+
+		$scope.C3 = function (){
+			Wad.midiInstrument.play({pitch : "C4"})
+		}
+		
+		$scope.Csharp3 = function (){
+			Wad.midiInstrument.play({pitch : "C#4"})
+		}
+		$scope.D3 = function (){
+			Wad.midiInstrument.play({pitch : "D4"})
+		}
+		$scope.Dsharp3 = function (){
+			Wad.midiInstrument.play({pitch : "D#4"})
+		}
+		$scope.E3 = function (){
+			Wad.midiInstrument.play({pitch : "E4"})
+		}
+		$scope.F3 = function (){
+			Wad.midiInstrument.play({pitch : "F4"})
+		}
+		$scope.Fsharp3 = function (){
+			Wad.midiInstrument.play({pitch : "F#4"})
+		}
+		$scope.G3 = function (){
+			Wad.midiInstrument.play({pitch : "G4"})
+		}
+		$scope.Gsharp3 = function (){
+			Wad.midiInstrument.play({pitch : "G#4"})
+		}
+		$scope.A3 = function (){
+			Wad.midiInstrument.play({pitch : "A4"})
+		}
+		$scope.Asharp3 = function (){
+			Wad.midiInstrument.play({pitch : "A#4"})
+		}
+		$scope.B3 = function (){
+			Wad.midiInstrument.play({pitch : "B4"})
+		}
 
 
 		
+
+                // <div class = "whitekey" ng-click="C3()">
+                // </div>
+                // <div class= "blackkey" ng-click="C#3()">
+                // </div>
+                // <div class = "whitekey" ng-click="D3()">
+                // </div>
+                // <div class= "blackkey" ng-click="D#3()">
+                // </div>
+                // <div class = "whitekey" ng-click="E3()">
+                // </div>
+                // <div class = "whitekey" ng-click="F3()">
+                // </div> 
+                // <div class= "blackkey" ng-click="F#3()">
+                // </div>
+                // <div class = "whitekey" ng-click="G3()">
+                // </div>
+                // <div class= "blackkey" ng-click="G#3()">
+                // </div>
+                // <div class = "whitekey" ng-click="A3()">
+                // </div>
+                // <div class= "blackkey" ng-click="A#3()">
+                // </div>
+                // <div class = "whitekey" ng-click="B3()">
+
+
+
+
 }])
 
 
